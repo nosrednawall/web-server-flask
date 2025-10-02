71458fe3ce2016f95e4e1f91f5374e1ab726d05b
nosrednawall <nosrednawall@gmail.com>
2025-10-02 15:10:08 -0300

    teste2

--- app.py
+++ app.py
@@ -1,8 +1,8 @@
-#!/usr/bin/env python3
-
+import os
 from project import create_app
 
 app = create_app()
 
 if __name__ == "__main__":
-    app.run()
+    port = int(os.environ.get("PORT", 10000))
+    app.run(host='0.0.0.0', port=port)
