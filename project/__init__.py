50cf5795ba236ef2b6842beae4bb66eabc297672
nosrednawall <nosrednawall@gmail.com>
2025-10-03 11:36:42 -0300

    add grid_nfe

--- project/__init__.py
+++ project/__init__.py
@@ -1,35 +1,38 @@
 import os
+# __init__.py (ou onde está create_app())
 from flask import Flask
 from flask_sqlalchemy import SQLAlchemy
 from flask_login import LoginManager
 
-# init SQLAlchemy so we can use it later in our models
 db = SQLAlchemy()
+login_manager = LoginManager()
 
 def create_app():
     app = Flask(__name__)
 
-    app.config['SECRET_KEY'] = 'secret-key-goes-here'
+    app.config['SECRET_KEY'] = 'sua-chave-secreta-aqui'
     app.config['SQLALCHEMY_DATABASE_URI'] = 'sqlite:///db.sqlite'
 
     db.init_app(app)
-
-    login_manager = LoginManager()
-    login_manager.login_view = 'auth.login'
     login_manager.init_app(app)
+    login_manager.login_view = 'auth.login'
+
+    from .models import User
 
     @login_manager.user_loader
     def load_user(user_id):
-        # since the user_id is just the primary key of our user table, use it in the query for the user
         return User.query.get(int(user_id))
 
-    from .models import User
-    # blueprint for auth routes in our app
+    # Blueprint de autenticação
     from .auth import auth as auth_blueprint
     app.register_blueprint(auth_blueprint)
 
-    # blueprint for non-auth parts of app
+    # Blueprint principal
     from .main import main as main_blueprint
     app.register_blueprint(main_blueprint)
+
+    # NOVO: Blueprint da Grid NF-e
+    from .grid_nfe import grid_nfe_bp
+    app.register_blueprint(grid_nfe_bp, url_prefix='/nfe')
 
     return app
